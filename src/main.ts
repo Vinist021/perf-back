fcbeb23a851b8ed9d70735dc1ea3e0dde5bb2852
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2026-02-01 12:52:45 -0300

    feat(swagger): adiciona documentação Swagger e dependências
    - Adiciona @nestjs/swagger e swagger-ui-express
    - Configura Swagger em src/main.ts com rota /api
    - Objetivo: documentação interativa da API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Perf-API')
+    .setDescription('Documentação da API do Perfil')
+    .setVersion('1.0')
+    .build();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +19,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
